1fbdb279180885788ad6f571643de5f921b3b481
michael.brouchoud <michael.brouchoud@heig-vd.ch>
2017-04-25 22:40:02 +0200

    Obliger de tout mettre pour compiler sur TeamCity

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,35 +1,29 @@
 package sample;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-/**
- * Created by elien on 11.04.2017.
- */
-
-import com.sun.security.ntlm.NTLMException;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-//        primaryStage.setTitle("Hello World");
-//        primaryStage.setScene(new Scene(root, 300, 275));
-//        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.show();
+		Client monClient = new Client();
     }
 
 
-    public static void main(String[] args) throws NTLMException {
-        //launch(args);
-        Client monClient = new Client();
+    public static void main(String[] args) {
+        launch(args);
     }
 }
