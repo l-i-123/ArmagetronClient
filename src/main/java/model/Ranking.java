4d1dfc931811daabb39051a85e0ff5309dc914de
Unknown <kevin.pradervand@heig-vd.ch>
2017-05-09 13:37:00 +0200

    coucou

--- src/main/java/model/Ranking.java
+++ src/main/java/model/Ranking.java
@@ -2,7 +2,10 @@ package model;
 
 /**
  * Created by Kevin on 04.05.2017.
+ * classe contenant le classement de la partie
  */
 public class Ranking {
+    //attention ajouter le nom dans les armagetronshare
+
 
 }
